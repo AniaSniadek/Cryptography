47090c91fe9ca64b1d8bea32f62c1607da1a079b
AniaSniadek <annsni2@st.amu.edu.pl>
2020-11-10 11:13:28 +0100

    add 3 exercise

---
+++ lab1/zad3.py
@@ -0,0 +1,12 @@
+def potegowanie(x, k, n):
+    y = 1
+    binary = list((bin(k)[2])[::-1])
+    l = len(binary)
+    i = l - 1
+
+    while i >= 0:
+        y = y**2 % n
+        if binary[i] == 1:
+            y = y*x % n
+        i = i - 1
+    return y
