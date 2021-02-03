a7571228aeabd553e5116381a49137eefb64cc48
AniaSniadek <annsni2@st.amu.edu.pl>
2021-02-03 15:57:18 +0100

    small changes

--- generator.py
+++ generator.py
@@ -31,28 +31,3 @@ def getLowLevelPrime(n):
 def generowanie(k):
     return getLowLevelPrime(k)
             
-  
-# def isMillerRabinPassed(mrc): 
-#     '''Run 20 iterations of Rabin Miller Primality test'''
-#     maxDivisionsByTwo = 0
-#     ec = mrc-1
-#     while ec % 2 == 0: 
-#         ec >>= 1
-#         maxDivisionsByTwo += 1
-#     assert(2**maxDivisionsByTwo * ec == mrc-1) 
-  
-#     def trialComposite(round_tester): 
-#         if pow(round_tester, ec, mrc) == 1: 
-#             return False
-#         for i in range(maxDivisionsByTwo): 
-#             if pow(round_tester, 2**i * ec, mrc) == mrc-1: 
-#                 return False
-#         return True
-  
-#     # Set number of trials here 
-#     numberOfRabinTrials = 20 
-#     for i in range(numberOfRabinTrials): 
-#         round_tester = random.randrange(2, mrc) 
-#         if trialComposite(round_tester): 
-#             return False
-#     return True
